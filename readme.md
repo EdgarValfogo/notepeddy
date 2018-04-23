821254e61d0a367a95bf0ee9b9653ebcadf93cec
Edgar Valfogo <edgarvalfogo@outlook.com>
2018-04-23 16:05:26 -0300

    :sparkles: Versão com o GET e POST funcionando corretamente. :construction: PUT e DELETE em andamento

--- readme.md
+++ readme.md
@@ -10,4 +10,23 @@
 - Temos que reiniciar sempre o server?
 - npm install --save-dev nodemon
 - Adicionar o novo script no package.json
+```
+"scripts": {
+    "dev": "nodemon server.js"
+  },
+```
 - npm run dev
+- Vamos fazer o post
+- Mas, esse dado de retorno não faz sentido. Como ler o POST/Request.body?
+- npm install --save body-parser
+- O post agora pode ser lido com request.body
+- E agora, onde salvaremos os dados?
+- npm install --save mongodb
+- Onde vamos armazenar nossos dados? mlab.com
+- O Create/Post
+Houve post?
+Como retornar os dados?
+- O Get/Read
+Como renderizar os resultados com template engines (Jade, Pug, Nunjucks, Embedded JavaScript )
+Vamos utilizar o Embedded Javascript (EJS)
+npm install --save ejs
--- server.js
+++ server.js
@@ -1,24 +1,69 @@
-const nodemon = require("nodemon");
+/**
+ * Requires
+ */
 
-const express = require("express");
-const app = express();
+ // Reinicia automaticamente o servidor após alguma alteração
+const nodemon = require("nodemon")
+// Servidor Express
+const express = require("express")
+const app = express()
+// Body parser para podermos ler os dados submetidos
+const bodyparser = require("body-parser")
+// Adicionaremos o mongodb para gerenciar o nosso banco de dados
+const MongoClient = require("mongodb").MongoClient
+const mongouser = 'nerdzaotest' 
+const mongopass = 'nerdzaotest'
+const mongourl = `mongodb://${mongouser}:${mongopass}@ds131151.mlab.com:31151/notepeddy`
+// Variável que instanciara o banco de dados
+var db;
 
-// Utilizaremos arrow functions do ES6
-app.listen(3000, () => {
-    // Servidor express iniciado com sucesso
-    app.get("/", (request, response, next) => {
-        response.send( __dirname + "/app/index.html");
-    });
+/**
+ * Middlewares e Plugins
+ */
+// Adicionaremos o body-parser como middleware
+app.use(bodyparser.urlencoded({extended: true}))
+// Após instalar o EJS, vamos inseri-lo na nossa aplicação como uma view engine, um renderizador
+app.set('view engine', 'ejs')
 
-    app.post("/", (request, response, next) => {
+// Inicialização do banco de dados
+MongoClient.connect(mongourl, (err, client) => {
+    // Se houver erros
+    if (err) return console.log(err)
+    
+    // Em caso de sucesso
+    db = client.db('notepeddy') // Aqui conectamos a base de dados
 
-    });
-
-    app.put("/", (request, response, next) => {
-
-    });
-
-    app.delete("/", (request, response, next) => {
+    app.listen(3000, () => {
+        // Servidor express iniciado com sucesso
+        // READ
+        app.get("/", (request, response, next) => {
+            var locals;
+            db.collection("notas").find().toArray( function(err, result) {
+                response.render( __dirname + "/app/index.ejs", {notas: result});
+            });
+            //response.sendFile( __dirname + "/app/index.html")
+            
+        })
         
+        // CREATE
+        app.post("/", (request, response, next) => {
+            db.collection("notas").save(request.body, (err, result) => {
+                if (err) return console.log(err)
+            })
+
+            console.log(request.body);
+            response.redirect("/");
+        })
+        
+        // UPDATE
+        app.put("/", (request, response, next) => {
+            response.redirect("/");
+        })
+        
+        // DELETE
+        app.delete("/", (request, response, next) => {
+            response.redirect("/");
+        })
     })
-});
+})
+// Utilizaremos arrow functions do ES6
